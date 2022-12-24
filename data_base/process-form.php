a8fd0479182aed7d65bb57899fbdf66dd9d2d557
nazwa <bla@bla.pl>
2022-12-24 15:52:13 +0100

    textarea

--- data_base/process-form.php
+++ data_base/process-form.php
@@ -1,4 +1,9 @@
 <?php
 
-print_r($_POST);
+$name = $_POST["name"];
+$message = $_POST["message"];
+$priority = filter_input(INPUT_POST, "priority", FILTER_VALIDATE_INT);
+$type = filter_input(INPUT_POST, "type", FILTER_VALIDATE_INT);
+$terms = filter_input(INPUT_POST, "terms", FILTER_VALIDATE_BOOLEAN);
 
+var_dump($name, $message, $priority, $type, $terms);
